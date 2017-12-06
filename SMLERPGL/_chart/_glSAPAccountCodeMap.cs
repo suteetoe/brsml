ed86a876d2e8f8e755b36d1562657a993e5f158f
Unknown <sutee.toe@gmail.com>
2017-12-06 16:25:04 +0700

    add map sap code vs gl_chart_of_account

---
+++ SMLERPGL/_chart/_glSAPAccountCodeMap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SMLERPGL._chart
+{
+    public class _glSAPAccountCodeMap : MyLib._manageMasterCodeFull
+    {
+        public _glSAPAccountCodeMap()
+        {
+            this._addColumn(_g.d.ic_shelf._code, 10, 20);
+            this._inputScreen._textBoxSearch += _inputScreen__textBoxSearch;
+            this._finish();
+
+        }
+
+        private void _inputScreen__textBoxSearch(object sender)
+        {
+         
+        }
+    }
+}
