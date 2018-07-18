550e767d3279e5f44628db3b7351e7298a8144ba
suteetoe <sutee.toe@gmail.com>
2018-07-18 10:20:21 +0700

    new field trans

--- SMLEDIControl/data/transdatasap.cs
+++ SMLEDIControl/data/transdatasap.cs
@@ -38,6 +38,8 @@ namespace SMLEDIControl
         public string PAYMENTTERM_CAL { get; set; }
         public string cust_code { get; set; }
 
+        public int vat_type { get; set; }
+        public int inquiry_type { get; set; }
         public transdatasap()
         {
 
@@ -69,7 +71,72 @@ namespace SMLEDIControl
 
         public string _queryInsert()
         {
-            return "";
+            StringBuilder sqlTrans = new StringBuilder();
+            sqlTrans.Append(
+                "INSERT INTO ic_trans(creator_code, trans_flag, trans_type" +
+                ", doc_no, doc_date, cust_code, sale_code" +
+                ", total_value, total_after_vat, total_amount, total_before_vat, total_discount, total_except_vat, total_vat_value, vat_rate" +
+                ", is_cancel, doc_time, tax_doc_date, tax_doc_no" +
+                ", inquiry_type, vat_type, doc_format_code" +
+                ", wh_from, location_from, wh_to, location_to, send_type" +
+                ", doc_ref, doc_ref_date , branch_code, last_status, credit_day"
+                    + ", credit_date, remark) ");
+            sqlTrans.Append(
+                " VALUES (\'?\', ?, ?" +
+                ", \'?\', \'?\', \'?\', \'?\'" +
+                ", ?, ?, ?, ?, ?, ?, ?, ?" +
+                ", ?, \'?\', \'?\', \'?\'" +
+                ", ?, ?, \'?\' " +
+                ", \'?\', \'?\', \'?\', \'?\', ?" +
+                ", \'?\', ?, \'?\', ?, ?"
+                    + ", ?, \'?\') "
+                    );
+
+            
+             
+             
+
+        StringBuilder sqlDetail = new StringBuilder();
+        sqlDetail.Append("INSERT INTO ic_trans_detail (" + 
+            "trans_flag, trans_type" + 
+            ", doc_no, doc_date, item_code, line_number, is_permium, unit_code, wh_code, shelf_code,"
+                + "qty, price, price_exclude_vat, sum_amount, discount_amount, total_vat_value, tax_type, vat_type"
+                + ", doc_time, calc_flag, sum_amount_exclude_vat"
+                + ", wh_code_2, shelf_code_2, branch_code" 
+                + ", inquiry_type, last_status, discount, cust_code)");
+        sqlDetail.Append(" VALUES (" + 
+            "?, ?, " + 
+            "?, ?,?, ?,?, ?,?, ?,?, ?,?, ?,?, ?,?,?"
+                + ", ?, ?, ?"
+                + ", ?, ?, ?"
+                + ", ?, ?, ?, ?) ");
+           
+
+
+            StringBuilder __queryInsert = new StringBuilder();
+                      
+
+            // ic_trans
+            __queryInsert.Append(MyLib._myUtil._convertTextToXmlForQuery(String.Format(sqlTrans.ToString()
+                , MyLib._myGlobal._userCode, 12, 1
+                , this.doc_no, this.doc_date, this.cust_code, ""
+                , this.total_value, this.total_after_vat, this.total_amount, this.total_before_vat, total_discount, total_except_vat, total_vat_value, vat_rate
+                0, doc_time, tax_doc_date, tax_doc_no
+                , inquiry_type, vat_type, doc_format_code
+                , wh_from, location_from, "", "", 0
+                , BILLINGDOCNO, "null", MyLib._myGlobal._branchCode, 0, credit_day
+                , ((credit_date.Length > 0) ? "\'" + credit_date + "\'" : "null"), remark
+                )));
+
+            // __queryInsert.Append(MyLib._myUtil._convertTextToXmlForQuery());
+
+            // detail
+            __queryInsert.Append(MyLib._myUtil._convertTextToXmlForQuery(String.Format(sqlDetail.ToString()
+                , 12, 1
+
+                )));
+
+            return __queryInsert.ToString();
         }
     }
 }
