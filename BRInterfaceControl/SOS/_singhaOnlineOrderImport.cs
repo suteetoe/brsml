0890bf2f3e232c56a3b00356f7ca97b4ae258e74
Nassarut  takawe <neverliken@gmail.com>
2018-08-14 14:49:41 +0700

    no message

--- BRInterfaceControl/SOS/_singhaOnlineOrderImport.cs
+++ BRInterfaceControl/SOS/_singhaOnlineOrderImport.cs
@@ -383,7 +383,10 @@ namespace BRInterfaceControl.SOS
                                         }
                                         else
                                         {
-                                            __itemName = (__items[__rowDetail].ContainsKey("item_name") && __items[__rowDetail]["item_name"] == null) ? "" : __items[__rowDetail]["item_name"].ToString().Replace("\"", string.Empty);
+                                            if (__items[__rowDetail].ContainsKey("item_name")) {
+                                                __itemName = ( __items[__rowDetail]["item_name"] == null) ? "" : __items[__rowDetail]["item_name"].ToString().Replace("\"", string.Empty);
+                                            }
+                                          //  __itemName = (__items[__rowDetail].ContainsKey("item_name") && __items[__rowDetail]["item_name"] == null) ? "" : __items[__rowDetail]["item_name"].ToString().Replace("\"", string.Empty);
                                         }
 
                                         string __insertDetail = "insert into " + _g.d.ic_trans_detail._table +
